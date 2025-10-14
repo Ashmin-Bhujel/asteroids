55d0c3b260a83e2248126f95ea25695342696fd7
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-10-14 20:57:21 +0545

    feat: game loop setup

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+# Entrypoint
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}")
+
+    # Initialize pygame
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    # Game loop
+    while True:
+        # Logic to quit the game when clicked the close button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
