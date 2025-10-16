a0d2637f3cd39082980aa8fa33258f00d073d575
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-10-16 23:09:42 +0545

    feat: asteroid split and score functionality

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from shot import Shot
 
 # Entrypoint
 def main():
+    print("=" * 20)
     print("Starting Asteroids!")
+    print("=" * 20)
     print(f"Screen Width: {SCREEN_WIDTH}")
     print(f"Screen Height: {SCREEN_HEIGHT}")
+    print("=" * 20)
 
     # Initialize pygame
     pygame.init()
@@ -19,6 +22,8 @@ def main():
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
+    font = pygame.font.Font(None, 32)
 
     # Create sprite groups
     updatable = pygame.sprite.Group()
@@ -42,18 +47,23 @@ def main():
         # Update all the updatable objects
         updatable.update(dt)
         screen.fill("#111111")
+        score_text = font.render(f"Score: {score}", True, "#111111", "#fafafa")
+        screen.blit(score_text, (10, 10))
 
         # Detect collisions
         for asteroid in asteroids:
             # Between asteroids and player
             if asteroid.is_collided(player):
+                print("=" * 20)
                 print("Game Over!")
+                print(f"Your Score: {score}")
+                print("=" * 20)
                 sys.exit(1)
 
             # Between asteroids and shots
             for shot in shots:
                 if asteroid.is_collided(shot):
-                    asteroid.kill()
+                    score += asteroid.split()
                     shot.kill()
 
         # Draw all the drawable objects
